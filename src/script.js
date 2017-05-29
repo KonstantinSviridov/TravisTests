72e8d7bf3034e21075f7e7fd905a843d5dbced46
KonstantinSviridov <ksviridov@gmail.com>
2017-05-29 22:07:29 +0600

    multiple files

--- src/script.js
+++ src/script.js
@@ -2,11 +2,24 @@ var parser = require("../../parser/raml-js-parser-2/dist/index");
 var path = require("path");
 
 var parserDir = path.resolve(__dirname,"../../parser/raml-js-parser-2");
-var testRelPath = "./src/raml1/test/data/parser/jsonscheme/test13/api.raml";
-var inputPath = path.resolve(parserDir,testRelPath);
 
-var api = parser.loadApiSync(inputPath);
-var expanded = api.expand();
-var json = expanded.toJSON({ rootNodeDetails: true });
+var testRelPaths = [
+  "./src/raml1/test/data/parser/jsonscheme/test13/api.raml",
+  "./TCK/raml-0.8/InvalidIncludeTags/test001/api.raml",
+  "./TCK/raml-0.8/InvalidIncludeTags/test002/api.raml",
+  "./TCK/raml-1.0/Examples/test059/api.raml",
+  "./TCK/raml-1.0/InvalidIncludeTags/test001/api.raml",
+  "./TCK/raml-1.0/InvalidIncludeTags/test002/api.raml"
+];
 
-console.log(JSON.stringify(json,null,2));
+var inputPaths = testPaths.map(function(x){return path.resolve(parserDir,x));
+
+for(var inputPath of inputPaths){
+	
+	console.log("Testing " + inputPath);
+	var api = parser.loadApiSync(inputPath);
+	var expanded = api.expand();
+	var json = expanded.toJSON({ rootNodeDetails: true });
+	
+	console.log(JSON.stringify(json,null,2));
+}
