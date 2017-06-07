ecdf5b8bf23e563d6652a9347d2b2c6616b25fb7
KonstantinSviridov <ksviridov@gmail.com>
2017-06-08 01:36:41 +0600

    new test

--- src/script.js
+++ src/script.js
@@ -4,12 +4,7 @@ var path = require("path");
 var parserDir = path.resolve(__dirname,"../../parser/raml-js-parser-2");
 
 var testRelPaths = [
-  "./src/raml1/test/data/parser/jsonscheme/test13/api.raml",
-  "./TCK/raml-0.8/InvalidIncludeTags/test001/api.raml",
-  "./TCK/raml-0.8/InvalidIncludeTags/test002/api.raml",
-  "./TCK/raml-1.0/Examples/test059/api.raml",
-  "./TCK/raml-1.0/InvalidIncludeTags/test001/api.raml",
-  "./TCK/raml-1.0/InvalidIncludeTags/test002/api.raml"
+  "./src/raml1/test/data/parser/examples/ex46.raml"
 ];
 
 var inputPaths = testRelPaths.map(function(x){return path.resolve(parserDir,x)});
